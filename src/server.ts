18baf33e5774926cfd13ff83d1c581b14af1343c
Legacynnn <danmelomour@gmail.com>
2021-06-28 03:10:48 -0300

    finish

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
-import express from 'express'
+import "reflect-metadata"
+import express, {Request, Response, NextFunction} from 'express'
+import "express-async-errors"
+
+import {router} from './routes'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.json({Server: "Server is running correctly"})
-})
+import './database'
 
-app.post('/test/post', (req, res) => {
-  res.json({Server: "Hello nlw"})
+app.use(express.json())
+
+app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
 })
 
 app.listen(3000, () => {
